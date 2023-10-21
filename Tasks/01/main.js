8aaf636028fa2b26685c413131e11152e347dc93
dev-pooya <pooya.sadeghi1997@gmail.com>
2023-10-21 22:09:30 +0330

    task 1

---
+++ Tasks/01/main.js
@@ -0,0 +1,10 @@
+// تمرین ۱
+
+// 1 , 2
+let students = ["ali", "mahdi", "حسین", [12, "hassan", 18, "167"]];
+
+// 3
+let message = "دوره آموزش برنامه نویسی مقدماتی";
+
+// 4
+let studentsCount = 22;
