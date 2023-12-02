6c027d1ac77d4cc6b8cf5a70b090845a30e89755
dev-pooya <pooya.sadeghi1997@gmail.com>
2023-12-02 14:00:49 +0330

    task 3

---
+++ Tasks/03/main.js
@@ -0,0 +1,134 @@
+console.log("task 3");
+// لیست دانش آموزان
+const students = [
+  {
+    fullname: "monica jones",
+    age: 18,
+    classes: ["js", "python"],
+    address: {
+      city: "maku",
+      street: "emam",
+      code: 334,
+    },
+  },
+  {
+    fullname: "mike martin",
+    age: 19,
+    classes: ["html", "js"],
+    address: {
+      city: "khoy",
+      street: "emam",
+      code: 220,
+    },
+  },
+  {
+    fullname: "mason smith",
+    age: 18,
+    classes: ["css", "python", "php"],
+    address: {
+      city: "maku",
+      street: "bolvar",
+      code: 130,
+    },
+  },
+  {
+    fullname: "ben smith",
+    age: 22,
+    classes: ["html"],
+    address: {
+      city: "yazd",
+      street: "baharan",
+      code: 425,
+    },
+  },
+  {
+    fullname: "jack parker",
+    age: 24,
+    classes: ["python"],
+    address: {
+      city: "tehran",
+      street: "hemat",
+      code: 635,
+    },
+  },
+  {
+    fullname: "john martin",
+    age: 20,
+    classes: ["js", "css"],
+    address: {
+      city: "yazd",
+      street: "golha",
+      code: 187,
+    },
+  },
+  {
+    fullname: "alex jones",
+    age: 23,
+    classes: ["html", "css"],
+    address: {
+      city: "tabriz",
+      street: "valiasr",
+      code: 236,
+    },
+  },
+  {
+    fullname: "sara foster",
+    age: 19,
+    classes: ["css"],
+    address: {
+      city: "tehran",
+      street: "emam",
+      code: 124,
+    },
+  },
+  {
+    fullname: "adam smith",
+    age: 17,
+    classes: [],
+    address: {
+      city: "maku",
+      street: "emam",
+      code: 136,
+    },
+  },
+  {
+    fullname: "tim wilson",
+    age: 25,
+    classes: ["html", "js", "css"],
+    address: {
+      city: "bane",
+      street: "emam",
+      code: 136,
+    },
+  },
+];
+
+// SORTING
+
+// 1
+
+// 2
+
+// 3
+
+// FILTERING
+
+// برای فیلتر کردن از تابع زیر استفاده کنید که در جلسه ۹ آموزش داده شد
+// تابع را از حالت کامنت خارج کنید  Ctrl + /
+
+// function filter(arr, callback) {
+//   let result = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (callback(arr[i])) {
+//       result.push(arr[i]);
+//     }
+//   }
+//   return result;
+// }
+
+// 4
+
+// 5
+
+// 6
